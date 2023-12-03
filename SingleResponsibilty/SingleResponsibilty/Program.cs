e19aa4ea6c56a59794e0cfdc002e9e41b32be8ce
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-12-03 16:46:49 +0400

    Add files via upload

---
+++ SingleResponsibilty/SingleResponsibilty/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SingleResponsibilty;
+
+
+public class Customer
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+}
+
+public class CustomerRepository
+{
+    public void SaveCustomer(Customer customer)
+    {
+        Console.WriteLine("Saved: " + customer.Name);
+    }
+}
+
+public class EmailService
+{
+    public void SendEmail(Customer customer, string message)
+    {
+        Console.WriteLine("E-mail sent: " + message + " - Buyer: " + customer.Email);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var customer = new Customer { Name = "Javid Gasim", Email = "gasimovcavid777@gmail.com" };
+
+        var customerRepository = new CustomerRepository();
+        customerRepository.SaveCustomer(customer);
+
+        var emailService = new EmailService();
+        emailService.SendEmail(customer, "Hello, Welcom!");
+    }
+}
